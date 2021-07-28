33f7296e33d6f9ef5c56f85c17082b6f280a790a
github username <github email address>
2021-07-28 16:24:16 -0700

    refactored by replacing controllerRouter to AppRouter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { router } from './routes/loginRoutes';
 import cookieSession from 'cookie-session';
-import { router as controllerRouter } from './controllers/decoractors/controllers';
+import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
 
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded());
 app.use(cookieSession({ keys: ['string'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
