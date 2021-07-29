5ec24ee0c195a9833fdab13b3ca2b6e385fc9b75
github username <github email address>
2021-07-28 18:04:18 -0700

    added bodyValidator to automate validations

--- src/controllers/decoractors/controllers.ts
+++ src/controllers/decoractors/controllers.ts
@@ -1,7 +1,26 @@
 import 'reflect-metadata';
 import { AppRouter } from '../../AppRouter';
+import { NextFunction, RequestHandler, Request, Response } from 'express';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+
+function bodyValidators(keys: string): RequestHandler {
+	return function (req: Request, res: Response, next: NextFunction) {
+		if (!req.body) {
+			res.status(422).send('Invalid request');
+			return;
+		}
+
+		for (let key of keys) {
+			if (!req.body[key]) {
+				res.status(422).send('Invalid request');
+				return;
+			}
+		}
+
+		next();
+	};
+}
 
 export function controller(routePrefex: string) {
 	return function (target: Function) {
@@ -25,11 +44,20 @@ export function controller(routePrefex: string) {
 					target.prototype,
 					key
 				) || [];
+			const requiredBodyProps =
+				Reflect.getMetadata(
+					MetadataKeys.validator,
+					target.prototype,
+					key
+				) || [];
+
+			const validator = bodyValidators(requiredBodyProps);
 
 			if (path) {
 				router[method](
 					`${routePrefex}${path}`,
 					...middlewares,
+					validator,
 					routeHandler
 				);
 			}
